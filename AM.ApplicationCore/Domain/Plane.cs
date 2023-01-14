f61595b68a5a8e23bc93ad2b5d78efe365441702
zayani.imen <zayani.imen@esprit.tn>
2023-01-14 17:25:20 +0100

    Add project files.

---
+++ AM.ApplicationCore/Domain/Plane.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Plane
+    {
+
+        //   public string Name;
+        public int PlaneId { get; set; }
+        public int Capacity { get; set; }
+        public DateTime ManufactureDate { get; set; }
+        public planeType PlanType { get; set; }
+
+    }
+}
